138449220fafdb3e8b5a7d2dd55ffba194051bd5
Benjamin Leeds <leeds@uw.edu>
2015-11-13 21:06:11 -0800

    add test folder for Protractor tests

---
+++ test/conf.js
@@ -0,0 +1,10 @@
+/* 
+* conf.js
+* Created: Friday November 13, 2015
+* Created By: J. Benjamin Leeds
+*/
+
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['*-spec.js']
+};
---
+++ test/demo-spec.js
@@ -0,0 +1,15 @@
+/*
+* demo-spec.js
+* Created: Friday November 13, 2015
+* Created By: J. Benjamin Leeds
+*/
+
+
+'use strict'
+
+describe('Our First Test', function(){
+
+	it('should test something', function(){
+		// test here
+	});
+})
