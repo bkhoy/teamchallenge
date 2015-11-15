efab2c292acad9c4ae2895b77c80938441c0505c
Brian Luu <briluu@uw.edu>
2015-11-15 15:31:24 -0800

    added proper email + first/last name validation

--- test/demo-spec.js
+++ test/demo-spec.js
@@ -7,9 +7,38 @@
 
 'use strict'
 
-describe('Our First Test', function(){
+describe('Sign-up validation page', function(){
 
-	it('should test something', function(){
-		// test here
+    beforeEach(function() {
+        // reload the page before each test
+        browser.get('http://localhost:8000');
+    });
+
+	it('should show an error message when email address is left blank after the user has touched it', function(){
+        $('#email').click(); // touch the email input
+        $('#firstName').click(); // touch something else and leave the input blank
+
+        expect( $('#forgotEmail').isDisplayed() ).toEqual(true);
 	});
+
+    it('should show an error message with INVALID email', function() {
+        $('#email').sendKeys('bad email haha xD');
+
+        expect( $('#invalidEmail').isDisplayed() ).toEqual(true);
+    });
+
+    it('should show no error message with VALID email', function() {
+        $('#email').sendKeys('goodemail@info343.com');
+        
+        expect( $('#invalidEmail').isDisplayed() ).toEqual(false);
+        expect( $('#forgotEmail').isDisplayed() ).toEqual(false);
+    });
+
+    it('should show an error message when last name text input is left blank after the user has touched it', function(){
+        $('#lastName').click(); // touch the last name input
+        $('#firstName').click(); // touch something else and leave the input blank
+
+        expect( $('#forgotLastName').isDisplayed() ).toEqual(true);
+    });
+
 })
